504760ab0d8d5dbc0d9fc6664a4c7f65450741ad
Alpesh <67307481+Alpeshpatel03434@users.noreply.github.com>
2021-11-28 22:48:15 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hospital Management System</title>
+
+    <link rel="stylesheet" href="css/index.css">
+
+</head>
+<body>
+
+    <img class=backgroud src="res/bg.png" alt="backgroud">
+    <div class="container">
+
+        <h1>Hospital Management System</h1>
+
+        <div class="contact">
+            <a href="./contact.php"><h2>Contact</h2></a>
+        </div>
+
+        <div class="home">
+            <a href="./index.html"><h2>Home</h2></a>
+        </div>
+
+    </div>
+
+    <div class="patientofhms">
+        
+        <img class=patient src="res/img2.png" alt="patient ican">
+        <a href="./hms/patient/user-login.php"><h2>Register & Book Appointment</h2></a>
+
+    </div>
+
+    <div class="doctorofhms">
+
+        <img class=doctor src="res/img3.png" alt="doctor ican">
+        <a class=dl href="./hms/doctor/index.php"><h2>Doctor's Login</h2></a>
+
+    </div>
+
+    <div class="adminfhms">
+
+        <img class=admin src="res/img4.png" alt="admin ican">
+        <a class=dl href="./hms/admin/index.php"><h2>Admin Login</h2></a>
+
+    </div>
+
+    <div class="footer">
+
+        <div class="Fcontact">
+            <a href="./contact.php"><h2>Contact</h2></a>
+        </div>
+
+        <div class="Fhome">
+            <a href="./index.html"><h2>Home</h2></a>
+        </div>
+
+    </div>
+
+</body>
+</html>
